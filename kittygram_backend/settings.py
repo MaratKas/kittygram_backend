2846044936dbdd4568785309974248ab1a366198
Марат Кашафразов <Mkashafrazov@gmail.com>
2022-12-11 23:35:55 +0500

    1

--- kittygram_backend/settings.py
+++ kittygram_backend/settings.py
@@ -19,18 +19,26 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework',
     'djoser',
+    'corsheaders',
     'cats.apps.CatsConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
+
+CORS_URLS_REGEX = r'^/api/.*$'
 
 ROOT_URLCONF = 'kittygram_backend.urls'
 
